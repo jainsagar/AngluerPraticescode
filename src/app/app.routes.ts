0a171f9c834622b4dad72ff330e78fe132336fdc
jainsagar <jainsagar1008@gmail.com>
2025-03-23 22:55:12 +0530

    add page 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { TolistComponent } from './tolist/tolist.component';
 import { ForpraticComponent } from './forpratic/forpratic.component';
+import { ErrorpageComponent } from './errorpage/errorpage.component';
 
 export const routes: Routes = [
     {path:"" , component:HomeComponent},
     {path:"Login",component:LoginComponent },
     {path:"TodoList",component:TolistComponent  },
     {path:"forLoop",component:ForpraticComponent  },
+    {path:"**", component:ErrorpageComponent}
 ];
